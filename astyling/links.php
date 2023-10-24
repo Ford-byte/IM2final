079391baa7c988c75e780b0192f2e2869af49985
Ford-byte <143000218+Ford-byte@users.noreply.github.com>
2023-10-24 21:11:30 +0800

    deleted

--- astyling/links.php
+++
@@ -1,5 +0,0 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-...the rest of your integrity code" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.all.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="../styling/index.js"></script>
-<link rel="stylesheet" href="../astyling/index.css">
